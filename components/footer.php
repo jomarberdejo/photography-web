06b16b09c5c9e4cd869f986562af5907fe5738df
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-12-19 23:00:44 +0800

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,14 +1,13 @@
-<!-- Footer -->
+
 <footer class="text-center text-lg-start bg-body-tertiary text-muted">
-  <!-- Section: Social media -->
+
   <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-    <!-- Left -->
+
     <div class="me-5 d-none d-lg-block">
       <span>Get connected with us on social networks:</span>
     </div>
-    <!-- Left -->
+   
 
-    <!-- Right -->
     <div>
       <a href="" class="me-4 text-reset">
         <i class="fab fa-facebook-f"></i>
@@ -29,11 +28,11 @@
         <i class="fab fa-github"></i>
       </a>
     </div>
-    <!-- Right -->
+   
   </section>
-  <!-- Section: Social media -->
 
-  <!-- Section: Links  -->
+
+
   <section class="">
     <div class="container text-center text-md-start mt-5">
       <!-- Grid row -->
@@ -48,11 +47,11 @@
            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatem inventore ex, illum doloribus enim dicta placeat sunt sequi non.
           </p>
         </div>
-        <!-- Grid column -->
+    
 
-        <!-- Grid column -->
+   
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
+        
           <h6 class="text-uppercase fw-bold mb-4">
             Products
           </h6>
@@ -69,9 +68,7 @@
             <a href="#!" class="text-reset">Lorem.</a>
           </p>
         </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
+     
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">
@@ -90,9 +87,7 @@
             <a href="#!" class="text-reset">Lorem</a>
           </p>
         </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
+        
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
@@ -104,13 +99,12 @@
           <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
           <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
         </div>
-        <!-- Grid column -->
+     
       </div>
-      <!-- Grid row -->
+
     </div>
   </section>
-  <!-- Section: Links  -->
+
 
  
 </footer>
-<!-- Footer -->
